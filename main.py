9e4e871a70b6d4ae1d463d44865de70668720838
Podogreff <vladkot92@gmail.com>
2022-10-01 23:39:53 +0300

    add

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def oops():
+    name = "vlad"
+    print(name[4])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+oops()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def new_oops():
+    name = "vlad"
+    try:
+        print(name[4])
+    except IndexError:
+        print("oops")
+
+
+new_oops()
